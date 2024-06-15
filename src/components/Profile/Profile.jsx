7ef24d01844888023bfc033a9dfb032c85ee2f7c
alex <al3xback@gmail.com>
2024-06-15 08:35:19 +0700

    updated prettier env

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,7 +9,7 @@ export default function Profile() {
 				className={styles.image}
 				width={80}
 				height={80}
-				alt='Jessica Randall'
+				alt="Jessica Randall"
 			/>
 			<div className={styles['title-box']}>
 				<h2 className={styles.title}>Jessica Randall</h2>
@@ -19,50 +19,50 @@ export default function Profile() {
 			<ul className={styles['social-links']}>
 				<li>
 					<a
-						href='#'
-						className='btn btn--primary btn--full'
-						rel='noopener'
-						target='_blank'
+						href="#"
+						className="btn btn--primary btn--full"
+						rel="noopener"
+						target="_blank"
 					>
 						GitHub
 					</a>
 				</li>
 				<li>
 					<a
-						href='#'
-						className='btn btn--primary btn--full'
-						rel='noopener'
-						target='_blank'
+						href="#"
+						className="btn btn--primary btn--full"
+						rel="noopener"
+						target="_blank"
 					>
 						Frontend Mentor
 					</a>
 				</li>
 				<li>
 					<a
-						href='#'
-						className='btn btn--primary btn--full'
-						rel='noopener'
-						target='_blank'
+						href="#"
+						className="btn btn--primary btn--full"
+						rel="noopener"
+						target="_blank"
 					>
 						LinkedIn
 					</a>
 				</li>
 				<li>
 					<a
-						href='#'
-						className='btn btn--primary btn--full'
-						rel='noopener'
-						target='_blank'
+						href="#"
+						className="btn btn--primary btn--full"
+						rel="noopener"
+						target="_blank"
 					>
 						Twitter
 					</a>
 				</li>
 				<li>
 					<a
-						href='#'
-						className='btn btn--primary btn--full'
-						rel='noopener'
-						target='_blank'
+						href="#"
+						className="btn btn--primary btn--full"
+						rel="noopener"
+						target="_blank"
 					>
 						Instagram
 					</a>
